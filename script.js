0e01e734b1b8a3a9c898eba31309e0540ca5d0fd
Dhiraj <nyamsharma1319@gmail.com>
2026-03-10 20:43:43 +0530

    Updated code

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ menuBtn.addEventListener("click", () => {
         sidebarOpen.style.display = "block";
         menuBtn.style.display = "none";
         sidebarClose.style.display = "block";
+        sidebarOpen.style.transition = "1s ease";
+
 
     }
 
@@ -38,5 +40,30 @@ sidebarClose.addEventListener("click", () => {
 
     }
 })
+
+
+
+
+
+async function download() {
+
+    const response = await fetch("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRNOf1O3PWNxq_z7Vzn-rePciR1eu7W1eyl4g&s");
+
+   const blob = await response.blob();
+    
+    const url = window.URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "file.pdf";
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
+
+
 
 
